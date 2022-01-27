68a32ce458ad58d20d8aae99d1b575a301f6c700
er-aryan <aryansharma200203@gmail.com>
2022-01-27 19:11:02 +0530

    add domain responses for b2 and b3

--- build-domain.py
+++ build-domain.py
@@ -2,7 +2,7 @@ import os
 import glob
 import hiyapyco
 
-path = '/home/love/Documents/SSA_Chatbot/domain-grp'
+path = '/home/user/Desktop/Sabudh/ResearchProject/SSA_Chatbot/domain-grp'
 yaml_list = []
 
 for filename in glob.glob(os.path.join(path, '*.yml')):
@@ -13,5 +13,5 @@ for filename in glob.glob(os.path.join(path, '*.yml')):
 merged_yaml = hiyapyco.load(yaml_list, method=hiyapyco.METHOD_MERGE)
 print(hiyapyco.dump(merged_yaml))
 
-domain_yml_file = open("/home/love/Documents/SSA_Chatbot/domain.yml","w+")
+domain_yml_file = open("/home/user/Desktop/Sabudh/ResearchProject/SSA_Chatbot/domain.yml","w+")
 domain_yml_file.writelines(hiyapyco.dump(merged_yaml))
