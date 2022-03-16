e94d5792993507430a3e3706e630f4261b947f8b
Lovepreet-Singh-ACET <123lovepreets@gmail.com>
2022-03-16 09:33:28 +0000

    rice b1

---
+++ Py_scripts/create_question_set_for_testing.py
@@ -0,0 +1,63 @@
+from fileinput import filename
+import yaml
+import os
+import pandas as pd
+
+nlu_file_names = [ "/home/ubuntu/SSA/SSA_Chatbot/data/nlu-faq-agri-rice-b1.yml" ]
+domain_file_names = ["/home/ubuntu/SSA/SSA_Chatbot/domain-grp/domain-agri-rice-b1.yml"]
+
+questions_list = []
+intent_names = []
+sentences = []
+for filename in nlu_file_names:
+    with open(filename) as file:
+        documents = yaml.full_load(file)
+        # print(documents)
+        for item, doc in documents.items():
+            if item == "nlu":
+                # print(item, ":")
+                for i in range(len(doc)):
+                    for sen in doc[i]["examples"].split("\n"):
+                        # print(sen[2:])
+                        # print()
+                        # print(doc[i]['intent'])
+                        if len(sen) > 1:    
+                            intent_names.append(doc[i]['intent'])
+                            sentences.append(sen[2:])
+                        # questions_list.append()
+                        # intent_names.append(doc[i]['intent'])
+print(len(intent_names))
+print(len(sentences))
+print("Domain")
+
+answer_list = []
+second_answer_list = []
+for filename in domain_file_names:        
+    with open(filename) as file:
+        documents = yaml.full_load(file)
+        # print(documents)
+        for item, doc in documents.items():
+            if item == "responses":
+                print(item, ":")
+                for intent in intent_names:
+                    try:
+                        # print(doc["utter_{}".format(intent)])
+                        if len(doc["utter_{}".format(intent)]) == 1:
+                            answer_list.append(doc["utter_{}".format(intent)][0]['text'])
+                            second_answer_list.append(" ")
+                        else:
+                            answer_list.append(doc["utter_{}".format(intent)][0]['text'])
+                            second_answer_list.append(doc["utter_{}".format(intent)][1]['text'])
+                    except:
+                        pass
+                    # print(doc[i]["examples"].split("\n")[0])
+
+print(len(answer_list))
+df = pd.DataFrame(
+    { "questoions": sentences,
+      "answers": answer_list
+      })
+df.to_csv("/home/ubuntu/SSA/SSA_Chatbot/CSV/test_set_rice_b1.csv", index=False)
+
+# print(len(questions_list), len(answer_list), len(second_answer_list))
+# print(answer_list)
---
+++ Py_scripts/testing.py
@@ -0,0 +1,49 @@
+"""
+This file is used to test the questions present in the faq spreadsheet
+"""
+import requests
+import json
+import pandas as pd
+
+data = pd.read_csv("/home/ubuntu/SSA/SSA_Chatbot/CSV/test_set_rice_b1.csv")
+# print(data)
+question_list = []
+original_answer_list = []
+bot_answer_list = []
+confidence_list = []
+correct_pred_or_not = []
+for index, row in data.iterrows():
+    question = row["questoions"]
+    orig_ans = row["answers"]
+
+    payload = json.dumps(dict(sender="rasa", message=question))
+    payload2 = json.dumps(dict(message_id="rasa", text=question))
+    # print(payload)
+    try:
+        r = requests.post( url='http://localhost:5005/webhooks/rest/webhook', data=payload)
+        r2 = requests.post( url='http://localhost:5005/model/parse', data=payload2)
+        # print(r)
+        # response = json.loads(r)
+        # print(r.text)
+        # print(r.json()[0]["text"])
+        # response = json.loads(r2)
+        # print(r2.json()['response_selector']['nlu-faq-weho-parking-b1']['response']['responses'][0]['text'])
+        _intent=r2.json()['intent']['name']
+        print(_intent)
+        confidence = r2.json()['response_selector'][_intent]['response']['confidence']
+        answer = r.json()[0]["text"]
+        question_list.append(question)
+        original_answer_list.append(orig_ans)
+        bot_answer_list.append(answer)
+        confidence_list.append(confidence)
+        correct_pred_or_not.append(answer == orig_ans)
+
+        print('predicted answer - ', answer)
+        print('original answer - ', orig_ans)
+        print('confidence -', confidence)
+    except:
+        pass
+
+df = pd.DataFrame({"question": question_list, "bot_answer": bot_answer_list, "orginal_answer": original_answer_list , "confidence": confidence_list, "Correct": correct_pred_or_not})
+
+df.to_csv("results.csv", index=False)
