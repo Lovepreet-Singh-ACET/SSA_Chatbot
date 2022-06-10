dcccb6f60702ddce2924b3c88f3af3686546cdfa
Lovepreet-Singh-ACET <123lovepreets@gmail.com>
2022-06-10 08:13:02 +0000

    updated wheat data

--- Py_scripts/Translated_Data_to_rasa_files.py
+++ Py_scripts/Translated_Data_to_rasa_files.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import pprint
 
+# File variables 
+input_file_name = "pun_data/rice_b4_punjabi.csv"
+nlu_output_filename = "data/nlu-faq-agri-rice-b4-pun.yml"
+domain_output_filename = "domain-grp/domain-agri-rice-b4-pun.yml"
+
+# Intant name variable
+intent_name = "faq-agri-rice-b4-pun"
+
 pp = pprint.PrettyPrinter(indent=4)
-data = pd.read_csv("pun_data/wheat_b5_punjabi.csv")
+data = pd.read_csv(input_file_name)
 
 # print(data.columns)
 # ['question', 'answer', 'Punjabi Question', 'Punjabi Answer']
@@ -15,9 +23,7 @@ df_sliced_dict = {}
 for ans in data['answer'].unique():
     df_sliced_dict[ans] = data[data['answer'] == ans]
 
-intent_name = "faq-agri-wheat-b5-pun"
-
-with open("data/nlu-faq-agri-wheat-b5-pun.yml", "w") as file, open("domain-grp/domain-agri-wheat-b5-pun.yml", "w") as file2:
+with open(nlu_output_filename, "w") as file, open(domain_output_filename, "w") as file2:
     file.write("version: \"3.0\"\n")
     file.write("nlu:\n")
 
@@ -27,22 +33,13 @@ with open("data/nlu-faq-agri-wheat-b5-pun.yml", "w") as file, open("domain-grp/d
     file2.write("responses:\n")
     for key in df_sliced_dict.keys():
         print(key)
-        # break
-        name = df_sliced_dict[key]['question'].iloc[0].strip().replace(" ", "-")                          #key.strip()[::5].replace(" ", "-")
+        name = df_sliced_dict[key]['question'].iloc[0].strip().replace(" ", "-")
         file.write(f"- intent: {intent_name}/{name}\n")
         file.write("  examples: |\n")
-
         file2.write(f"  utter_{intent_name}/{name}:\n")
-
-        # print(df_sliced_dict[key].columns)
-        # break
-        # for df_key in df_sliced_dict[key]:
         df_key = df_sliced_dict[key].copy()
-        # print(df_key)
-        # break
         lst = df_key['Punjabi Question'].tolist()
         string = "    - "+"\n    - ".join(lst) + "\n"
         file.write(string)
-
         answer = df_key["Punjabi Answer"].iloc[0]
         file2.write(f"  - text: {answer}\n")
--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,6 @@ from rasa_sdk.executor import CollectingDispatcher
 # from fuzzywuzzy import fuzz
 from textblob import TextBlob
 
-
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
@@ -34,6 +33,44 @@ from textblob import TextBlob
 
 #         return []
 
+class ActionGeneral(Action):
+
+    def name(self) -> Text:
+        return "action_general"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        print("********************action_general**********************\n\n")
+        _intent=tracker.latest_message['intent'].get('name')
+        print(_intent)
+        if _intent == "nlu_fallback":
+            dispatcher.utter_message(response="utter_please_rephrase", buttons=[{"payload": "", "title": ""}])
+        else:
+            dispatcher.utter_message(response="utter_" + str(_intent), buttons=[{"payload": "", "title": ""}])
+        return []
+
+# class ActionSlotSetter(Action):
+#     def name(self) -> Text:
+#         return "action_slot_setter"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         buttons = [
+#             {"payload":'/ok{"intent_button":"faq-agriculture"}',"title":"Agriculture"},
+#             {"payload":'/ok{"intent_button":"faq-horticulture"}',"title":"Horticulture"}
+#         ]
+#         print(tracker.slots['intent_button'], "------------------------------")
+#         if tracker.slots['intent_button'] == None:
+#             print("\n","slots value is ",tracker.slots['intent_button'])
+#             dispatcher.utter_message(text="Hi! Welcome to Farmerapp chatbot. For any queries related to crop, vegetation, etc, please select a topic from the below options.",buttons=buttons)
+#         else:
+#             print("\n","Now slots value is ",tracker.slots['intent_button'])
+#             dispatcher.utter_message(text="Yes you are good to go")
+#         return []
+
 class ActionSlotSetter(Action):
     def name(self) -> Text:
         return "action_slot_setter"
@@ -46,13 +83,116 @@ class ActionSlotSetter(Action):
             {"payload":'/ok{"intent_button":"faq-horticulture"}',"title":"Horticulture"}
         ]
         print(tracker.slots['intent_button'], "------------------------------")
-        if tracker.slots['intent_button'] == None:
-            print("\n","slots value is ",tracker.slots['intent_button'])
-            dispatcher.utter_message(text="Hi! Welcome to Farmerapp chatbot. For any queries related to crop, vegetation, etc, please select a topic from the below options.",buttons=buttons)
-        else:
-            print("\n","Now slots value is ",tracker.slots['intent_button'])
-            dispatcher.utter_message(text="Yes you are good to go")
+        # if tracker.slots['intent_button'] == None:
+        #     print("\n","slots value is ",tracker.slots['intent_button'])
+        #     dispatcher.utter_message(text="Hi!! Welcome to SSA Punjab Bot. How can I help you??",buttons=buttons)
+        # else:
+        #     print("\n","Now slots value is ",tracker.slots['intent_button'])
+        #     dispatcher.utter_message(text="Yes you are good to go")
+        print("\n","slots value is ",tracker.slots['intent_button'])
+        dispatcher.utter_message(text="Hi! Welcome to Farmerapp chatbot. For any queries related to crop, vegetation, etc, please select a topic from the below options.",buttons=buttons)
         return []
+
+# class ActionVizFaq(Action):
+
+#     def name(self) -> Text:
+#         return "action_viz_faq"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         # print(tracker.latest_message)
+#         buttons = [
+#             {"payload":'/ok{"intent_button":"faq-agriculture"}',"title":"Agriculture"},
+#             {"payload":'/ok{"intent_button":"faq-horticulture"}',"title":"Horticulture"}
+#         ]
+        
+#         # dictionary for mapped retrieval intents
+#         mapped_intent= { "faq-agriculture" : "Agriculture",
+#                         "faq-horticulture":"Horticulture",
+#                         None: "No-option"}
+
+#         # to get a slot value (here --> slot is intent_button)
+#         print("\n","Current Slots Value is: ",tracker.slots['intent_button']) 
+
+#         if tracker.slots['intent_button'] ==None:
+#             slot_value_clicked = mapped_intent[tracker.slots['intent_button']]
+#         elif tracker.slots['intent_button'] == 'faq-agri-wheat':
+#             slot_value_clicked = 'faq-agriculture'
+#         elif tracker.slots['intent_button'] == 'faq-horti-wheat':
+#             slot_value_clicked = 'faq-horticulture'
+#         else:
+#             slot_value_clicked = tracker.slots['intent_button']
+
+#         # to get intent of user message
+#         _intent=tracker.latest_message['intent'].get('name')
+#         print("\nIntent of user message predicted by Rasa: ",_intent)
+
+#         print("\nUser Message",tracker.latest_message['text']) # to get user typed message 
+
+#         intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
+
+#         second_intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][1]['intent_response_key'], indent=4)
+#         second_retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['ranking'][1]['confidence']*100
+#         # print('tracker latest message', tracker.latest_message['response_selector'])
+#         # print(' ')
+#         # print('---', tracker.latest_message['response_selector'].keys())
+#         # print('   ')
+#         print("\nretrieval we found (i.e intent response key ) ",intent_found)
+
+#         # confidence of retrieval intent we found
+#         retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
+#         print(f"\nRetrieval_intent_confidence we found was {retrieval_intent_confidence}")
+#         # print("\nSecond Intent: ", second_intent_found, "\nConfidence: ", second_retrieval_intent_confidence)
+#         if retrieval_intent_confidence < 80:
+#             dispatcher.utter_message(text="I couldn't understand. Can you please rephrase it?")
+#             return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ]
+
+        
+#         print("Intent: ", _intent,"Slot Clicked: ", slot_value_clicked[0])
+#         if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
+#             dispatcher.utter_message(text='You can fill and submit the Google form')
+
+#         elif _intent[4:8] == slot_value_clicked[0][4:8] :
+#             """ if intent found is same as faq-visualisation or faq-portal or any other category
+#             -3 tells we have left - and batch number 
+#             ex from faq-visualisation-b0 we took faq-visualisation """
+
+
+#         #used eval to remove quotes around the string
+#             # print('intent before adding the utter', intent_found)
+#             # print('----', eval(intent_found))
+#             intent_found = f'utter_{eval(intent_found)}'
+#             print('after adding utter we found -- ', intent_found)
+#             dispatcher.utter_message(response = intent_found) # use response for defining intent name
+#             # print("Difference: ",retrieval_intent_confidence - second_retrieval_intent_confidence)
+#             if retrieval_intent_confidence - second_retrieval_intent_confidence <= 5:
+#                 # print("in")
+#                 dispatcher.utter_message(text="One more possible solution could be as below")
+#                 dispatcher.utter_message(response=f'utter_{eval(second_intent_found)}')
+#                 dispatcher.utter_message(text="Take decison as per your choice")
+        
+#         elif slot_value_clicked == 'No-option':
+#             dispatcher.utter_message(text = "Please select any option first",buttons=buttons )
+        
+#         else:
+#             # if retrieval_intent_confidence > 90:
+#             intent_found = f'utter_{eval(intent_found)}'
+            
+#             dispatcher.utter_message(response = intent_found)
+#             if _intent[4:8] == 'hort':
+#                 domain = 'horticulture'
+#             else:
+#                 domain = 'agriculture' 
+#             dispatcher.utter_message(text = f"Seems like you want to ask question from {domain} domain If yes you are good to go with that  but if you want to ask question from any other category please select a button",buttons=buttons)
+            
+#             tracker.slots['intent_button'] = _intent[:-3]
+            
+#             print(f"\nNow slot value is {tracker.slots['intent_button']}","\n")
+#             print("---------------------------------------------------------")
+
+#         return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ] # setting slot values
 
 class ActionVizFaq(Action):
 
@@ -75,7 +215,7 @@ class ActionVizFaq(Action):
                         None: "No-option"}
 
         # to get a slot value (here --> slot is intent_button)
-        print("\n","Current Slots Value is: ",tracker.slots['intent_button']) 
+        print("\n","Under viz faq slots value is ",tracker.slots['intent_button']) 
         
         if tracker.slots['intent_button'] ==None:
             slot_value_clicked = mapped_intent[tracker.slots['intent_button']]
@@ -88,32 +228,32 @@ class ActionVizFaq(Action):
 
         # to get intent of user message
         _intent=tracker.latest_message['intent'].get('name')
-        print("\nIntent of user message predicted by Rasa: ",_intent)
+        print("Intent of user message predicted by Rasa ",_intent)
 
-        print("\nUser Message",tracker.latest_message['text']) # to get user typed message 
+        print(tracker.latest_message['text']) # to get user typed message 
 
         intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
         
         second_intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][1]['intent_response_key'], indent=4)
         second_retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['ranking'][1]['confidence']*100
+        print("Second Intent: ", second_intent_found, "\nConfidence: ", second_retrieval_intent_confidence)
         # print('tracker latest message', tracker.latest_message['response_selector'])
         # print(' ')
         # print('---', tracker.latest_message['response_selector'].keys())
         # print('   ')
-        print("\nretrieval we found (i.e intent response key ) ",intent_found)
+        print("retrieval we found (i.e intent response key ) ",intent_found)
 
         # confidence of retrieval intent we found
         retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
-        print(f"\nRetrieval_intent_confidence we found was {retrieval_intent_confidence}")
-        # print("\nSecond Intent: ", second_intent_found, "\nConfidence: ", second_retrieval_intent_confidence)
+        print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
         if retrieval_intent_confidence < 80:
-            dispatcher.utter_message(text="I couldn't understand. Can you please rephrase it?")
+            dispatcher.utter_message(text="I couldn't understand. Can you please rephrase it?", buttons=[{"payload": "", "title": ""}])
             return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ]
 
         
-        print("Intent: ", _intent,"Slot Clicked: ", slot_value_clicked[0])
+        print(_intent, slot_value_clicked[0])
         if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
-            dispatcher.utter_message(text='You can fill and submit the Google form')
+            dispatcher.utter_message(text='You can fill and submit the Google form', buttons=[{"payload": "", "title": ""}])
 
         elif _intent[4:8] == slot_value_clicked[0][4:8] :
             """ if intent found is same as faq-visualisation or faq-portal or any other category
@@ -126,22 +266,22 @@ class ActionVizFaq(Action):
             # print('----', eval(intent_found))
             intent_found = f'utter_{eval(intent_found)}'
             print('after adding utter we found -- ', intent_found)
-            dispatcher.utter_message(response = intent_found) # use response for defining intent name
-            # print("Difference: ",retrieval_intent_confidence - second_retrieval_intent_confidence)
+            dispatcher.utter_message(response = intent_found, buttons=[{"payload": "", "title": ""}]) # use response for defining intent name
+            print(retrieval_intent_confidence - second_retrieval_intent_confidence)
             if retrieval_intent_confidence - second_retrieval_intent_confidence <= 5:
-                # print("in")
-                dispatcher.utter_message(text="One more possible solution could be as below")
-                dispatcher.utter_message(response=f'utter_{eval(second_intent_found)}')
-                dispatcher.utter_message(text="Take decison as per your choice")
+                print("in")
+                dispatcher.utter_message(text="One more possible solution could be as below", buttons=[{"payload": "", "title": ""}])
+                dispatcher.utter_message(response=f'utter_{eval(second_intent_found)}', buttons=[{"payload": "", "title": ""}])
+                dispatcher.utter_message(text="Take decison as per your choice", buttons=[{"payload": "", "title": ""}])
         
         elif slot_value_clicked == 'No-option':
-            dispatcher.utter_message(text = "Please select any option first",buttons=buttons )
+            dispatcher.utter_message(text = "Please select any option first", buttons=buttons)
         
         else:
             # if retrieval_intent_confidence > 90:
             intent_found = f'utter_{eval(intent_found)}'
             
-            dispatcher.utter_message(response = intent_found)
+            dispatcher.utter_message(response = intent_found, buttons=[{"payload": "", "title": ""}])
             if _intent[4:8] == 'hort':
                 domain = 'horticulture'
             else:
@@ -150,8 +290,7 @@ class ActionVizFaq(Action):
             
             tracker.slots['intent_button'] = _intent[:-3]
             
-            print(f"\nNow slot value is {tracker.slots['intent_button']}","\n")
-            print("---------------------------------------------------------")
+            print(f"Now slot value is {tracker.slots['intent_button']}","\n")
 
         return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ] # setting slot values
 
